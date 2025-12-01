d22a707ed5364f3ce384815f14d058090ab16568
Ryan Godwin <ryan.godwin524@outlook.com>
2025-12-01 06:55:48 -0600

    Add project files.

---
+++ adventofcode2025/adventofcode2025.cpp
@@ -0,0 +1,12 @@
+﻿// adventofcode2025.cpp : Defines the entry point for the application.
+//
+
+#include "adventofcode2025.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

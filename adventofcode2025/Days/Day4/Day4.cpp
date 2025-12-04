a3845ba6862cce0af05ddf7ca0cd7ec2d9670073
Ryan Godwin <ryan.godwin524@outlook.com>
2025-12-04 16:06:56 -0600

    Added comments to Day 4

--- adventofcode2025/Days/Day4/Day4.cpp
+++ adventofcode2025/Days/Day4/Day4.cpp
@@ -8,11 +8,13 @@ void Day4::run() {
 	//rolls can be accessed IF there are fewer than 4 adjacent rolls.
 	//8 cardinal directions: n e s w ne se sw nw
 	// 
-	// part 1
-	//how many rolls can be accessed?
+	// part 1:
+	//		how many rolls are accessible?
+	// part 2:
+	//		remove accessible rolls until there are no more accessible rolls.
+	//		how many got removed?
 
 	std::vector<std::vector<char>> grid;
-	
 
 	while (getline(file, text)) {
 		std::vector<char> line;
@@ -22,18 +24,21 @@ void Day4::run() {
 		grid.push_back(line);
 	};
 	
+	//VERSION 1
 	result1 = Part1(grid);
 	result2 = Part2(grid);
 	
-	/*
+	/* VERSION 2
 	std::map<std::pair<int, int>, std::set<std::pair<int, int>>> graph;
 	result1 = Part1v2(grid, graph);
 	result2 = Part2v2(graph);
 	*/
 	std::cout << "Day 4: " << result1 << " " << result2 << std::endl;
 }
+
+//count how many rolls are accessible
 int Day4::Part1(std::vector<std::vector<char>>& grid) {
-	int changes = 0;
+	int accessibleRolls = 0;
 	for (size_t i = 0; i < grid.size(); ++i) {
 		for (size_t j = 0; j < grid[i].size(); ++j) {
 			if (grid[i][j] == '@') {
@@ -63,68 +68,18 @@ int Day4::Part1(std::vector<std::vector<char>>& grid) {
 						countAdjacent += grid[i + 1][j + 1] == '@';
 					}
 				}
-				if (countAdjacent < 4) {
-					changes++;
-				}
+				accessibleRolls += countAdjacent < 4;
 			}
 		}
 	}
-	return changes;
+	return accessibleRolls;
 }
 
-
-int Day4::Part1v2(std::vector<std::vector<char>>& grid, std::map<std::pair<int, int>, std::set<std::pair<int, int>>> &graph) {
-	
-	for (size_t i = 0; i < grid.size(); i++) {
-		for (size_t j = 0; j < grid[i].size(); j++) {
-			if (grid[i][j] == '@') {
-
-				//this line initializes the position in the graph.
-				//needed for cases where there are NO adjacent '@'.
-				graph[{i, j}];
-
-				if (j < grid[i].size() - 1
-					&& grid[i][j + 1] == '@') { //east east
-
-					graph[{i, j}].insert({ i, j + 1 });
-					graph[{i, j + 1}].insert({ i,j });
-				}
-
-				if (i < grid.size() - 1) { //south
-					if (j > 0
-						&& grid[i + 1][j - 1] == '@') { //south west
-
-						graph[{i, j}].insert({ i + 1, j - 1 });
-						graph[{i + 1, j - 1}].insert({ i, j });
-					}
-
-					if (grid[i + 1][j] == '@') { //south south
-
-						graph[{i, j}].insert({ i + 1, j });
-						graph[{i + 1, j}].insert({ i, j });
-					}
-
-					if (j < grid[i].size() - 1
-						&& grid[i + 1][j + 1] == '@') { //south east
-
-						graph[{i, j}].insert({ i + 1, j + 1 });
-						graph[{i + 1, j + 1}].insert({ i, j });
-					}
-				}
-			}
-		}
-	}
-	int accessibleRollCount = 0;
-	for (auto g : graph) {
-		accessibleRollCount += g.second.size() < 4;
-	}
-	return accessibleRollCount;
-}
-
-//same as part 1, 
-//but remove accessable rolls and 
-//check for newly accessable rolls 
-//until we cant access any rolls.
+/// <summary>
+///		Remove all accessible rolls from the grid.
+/// </summary>
+/// <param name="grid">Grid you want rolls removed from.</param>
+/// <returns>Number of rolls removed from the grid.</returns>
 int Day4::Part2(std::vector<std::vector<char>> &grid) {
 	int changeSum = 0;
 	int changes = 1;
@@ -171,7 +126,76 @@ int Day4::Part2(std::vector<std::vector<char>> &grid) {
 	return changeSum;
 }
 
+/*
+		VERSION 2:
+		Create a graph of the rolls in part 1.
+		Now we only have to iterate over rolls in part 2.
+		Skipping empty floor.
 
+		Creating the graph made this ~10x slower than version 1.
+		It did make part 2 faster.
+*/
+
+
+/// <summary>
+/// Count the amount of accessible rolls and create a graph of the rolls.
+/// </summary>
+/// <param name="grid">Grid of rolls to be counted.</param>
+/// <param name="graph"></param>
+/// <returns>Number of rolls that are accessible.</returns>
+int Day4::Part1v2(std::vector<std::vector<char>>& grid, std::map<std::pair<int, int>, std::set<std::pair<int, int>>>& graph) {
+
+	for (size_t i = 0; i < grid.size(); i++) {
+		for (size_t j = 0; j < grid[i].size(); j++) {
+			if (grid[i][j] == '@') {
+
+				//this line initializes the position in the graph.
+				//needed for cases where there are NO adjacent '@'.
+				graph[{i, j}];
+
+				if (j < grid[i].size() - 1
+					&& grid[i][j + 1] == '@') { //east east
+
+					graph[{i, j}].insert({ i, j + 1 });
+					graph[{i, j + 1}].insert({ i,j });
+				}
+
+				if (i < grid.size() - 1) { //south
+					if (j > 0
+						&& grid[i + 1][j - 1] == '@') { //south west
+
+						graph[{i, j}].insert({ i + 1, j - 1 });
+						graph[{i + 1, j - 1}].insert({ i, j });
+					}
+
+					if (grid[i + 1][j] == '@') { //south south
+
+						graph[{i, j}].insert({ i + 1, j });
+						graph[{i + 1, j}].insert({ i, j });
+					}
+
+					if (j < grid[i].size() - 1
+						&& grid[i + 1][j + 1] == '@') { //south east
+
+						graph[{i, j}].insert({ i + 1, j + 1 });
+						graph[{i + 1, j + 1}].insert({ i, j });
+					}
+				}
+			}
+		}
+	}
+	int accessibleRolls = 0;
+	for (auto g : graph) {
+		accessibleRolls += g.second.size() < 4;
+	}
+	return accessibleRolls;
+}
+
+/// <summary>
+///		Remove accessible rolls from the graph.
+/// </summary>
+/// <param name="graph">The graph you want the rolls removed from.</param>
+/// <returns>Number of rolls removed.</returns>
 int Day4::Part2v2(std::map<std::pair<int, int>, std::set<std::pair<int, int>>>& graph) {
 	int startSize = graph.size();
 	
